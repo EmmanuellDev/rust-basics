ec90744a5c957845f06b9ddaf308a8a70d106cdf
EmmanuellDev <emmanuelsk04@gmail.com>
2024-12-12 12:48:04 +0530

    data types, comments, operators

--- basic-1.rs
+++ basic-1.rs
@@ -1,5 +1,115 @@
-//VARIABLES
-let x = 5; // immutable variable
-let mut y = 10; //mutable variable
+fn main() {
+    //VARIABLES
+    let x = 5; // immutable variable
+    let mut y = 10; //mutable variable
+    println!("{}", y);
+    y = 9;
+    println!("{}", y);
 
-//
+    //DATA TYPES
+    // Scalar Type
+    // Integer
+    let x1: i8 = 3;
+    let x2: i16 = 3;
+    let x3: i32 = 3;
+    let x4: i64 = 3;
+    let x5: i128 = 3;
+    let x6: isize = 3;
+
+    println!("{x}");
+    println!("{x1}");
+    println!("{x2}");
+    println!("{x3}");
+    println!("{x4}");
+    println!("{x5}");
+    println!("{x6}");
+
+    // Floating Point
+    let v1: f32 = 7.5;
+    let v2: f64 = 7.5;
+
+    println!("{v1}");
+    println!("{v2}");
+
+    // Boolean
+    let ismen: bool = true;
+
+    println!("{ismen}");
+
+    // Character
+    let s: char = 'E';
+
+    println!("{s}");
+
+    // String
+    let ss: &str = "I am Emmanuel";
+
+    println!("{ss}");
+    // Compound Type
+    // Tuples
+
+    let person: (&str, i32, f64) = ("Alice", 30, 5.5);
+    println!("Name: {}, Age: {}, Height: {}", person.0, person.1, person.2);
+
+    // Array
+    let numbers = [10, 20, 30, 40, 50];
+    println!("First Number: {},{},{}", numbers[0], numbers[3], numbers[2]);
+    println!("All Numbers: {:?}", numbers);
+
+    // COMMENTS
+    // This is a single-line comment
+
+    /* This
+    is
+    a
+    multi-line
+    comment */
+    
+    // OPERATORS
+    let mut a: i32 = 5;
+    let b: i32 = 7;
+    let a1: bool = true;
+    let a2: bool = false;
+
+    // Arithmetic Operator
+    println!("{} + {} = {}",a, b, a+b); // Addition
+    println!("{} + {} = {}",a, b, a-b); // Subtraction
+    println!("{} + {} = {}",a, b, a*b); // Multiplication
+    println!("{} + {} = {}",a, b, a/b); // Division
+    println!("{} + {} = {}",a, b, a%b); // Modulus
+
+    // Comparison Operator
+    println!("{}", a == b);
+    println!("{}", a != b);
+
+    // Logical Operator
+    println!("{}", a1&&a2);
+    println!("{}", a1||a2);
+    println!("{}", !a1);
+
+    // Bitwise Operator
+    println!("{}", a&b);
+    println!("{}", a|b);
+    println!("{}", a^b);
+    println!("{}", a<<b);
+    println!("{}", a>>b);
+
+    // Assignment Operator
+    a+=b;
+    println!("{a}");
+    a-=b;
+    println!("{a}");
+    a*=b;
+    println!("{a}");
+    a/=b;
+    println!("{a}");
+    a%=b;
+    println!("{a}");
+
+    // Miscellaneous Operator
+    let abc = ['2','4','6'];
+    println!("{}", abc.len());
+    // -> pointer
+    // 1..5 range
+
+}
